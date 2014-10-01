2b50110ac3913eb5a485bc50b7b5f71ae1d2ab25
elainekamlley <elaine@codeforprogress.org>
2014-10-01 15:05:09 -0400

    Added email code but not working, need to fix

--- app/views.py
+++ app/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.utils.decorators import method_decorator
 from django.core.urlresolvers import reverse
 from django.views.generic.base import TemplateView
+from django.core.mail import send_mass_mail
+from email_texts import admin_email_address
 from app.models import Contact
 from app.forms import ContactForm
 
@@ -28,6 +30,9 @@ def press(request):
 def fellows(request):
     return render(request, "fellows.html")
 
+def thank_you(request):
+    return render(request, "thankyou.html")
+
 class ContactUsView(TemplateView):
     def get(self, request, **kwargs):
         form = ContactForm()
@@ -46,12 +51,38 @@ class ContactUsView(TemplateView):
                 if first_name and last_name and email and interested_in:
                     contact = Contact(first_name = first_name, last_name = last_name, email = email, interested_in = interested_in)
                     contact.save()
-                return render (request, 'thankyou.html', {'form': form})
+                # return render (request, 'thankyou.html', {'form': form})
+
+                from email_texts import english_version_emails as emails
+
+                # Grab admin email list (if not already grabbed or stored somewhere else)
+                admin_email_list = [admin_email_address]
+        
+                # Build confirmation email
+                email = emails['contact']['confirmation']
+                email_body = email['body']#.format(first_name=contact.first_name,
+                #     email=contact.email,
+                #     interested_in=contact.interested_in)
+                confirmation_email = (email['subject'], email_body, email['from'], [contact.email])
+        
+                # Build admin notification email
+                email = emails['contact']['admin']
+                admin_email_body = email['body']#.format(email=contact.email)
+                admin_email = (email['subject'], admin_email_body, email['from'], admin_email_list)
+        
+                # Send Them
+                try:
+                    send_mass_mail((admin_email, confirmation_email), fail_silently=False)
+                except:
+                    pass
+                #end of code for confirmation e-mail
+                
+                
+                # Still need to check for saving of skills they have here
+                return HttpResponseRedirect(reverse('thank_you'))
 
             else: 
                 form = ContactForm()
                 return render(request, 'contact.html', {'form' : form})
 
 
-def thank_you(request):
-    return render(request, "thankyou.html")
