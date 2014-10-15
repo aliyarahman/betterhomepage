e25f927258732bf752d2383b5f89e4ae6783d58a
elainekamlley <elaine@codeforprogress.org>
2014-10-15 12:50:26 -0400

    fixed all form requirements: spaces, added options, button

--- app/forms.py
+++ app/forms.py
@@ -9,8 +9,10 @@ from django.forms.models import modelformset_factory
 CONTACT_CHOICES =   (('application', ('receive the 2015 application when it is released.')),
                      ('coach', ('be a coding or professional development coach for new coders or fellows.')),
                      ('instructor', ('teach as a guest instructor.')),
-                     ('mentor', ('be a year-long professional mentor.')),)
-
+                     ('mentor', ('be a year-long professional mentor.')),
+                     ('email_list', ('join the email list and receive updates on Code For Progress opportunities.')),
+                     ('other', ('contact Code for Progress for another matter.'))
+                     )
 
 
 class ContactForm(forms.Form):
