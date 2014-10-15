31c738df5235ce4759eb1650f904b3129f842f6b
KittyLee <katherine@codeforprogress.org>
2014-10-15 11:23:24 -0400

    basic updates to base

--- app/forms.py
+++ app/forms.py
@@ -9,7 +9,8 @@ from django.forms.models import modelformset_factory
 CONTACT_CHOICES =   (('application', ('receive the 2015 application when it is released.')),
                      ('coach', ('be a coding or professional development coach for new coders or fellows.')),
                      ('instructor', ('teach as a guest instructor.')),
-                     ('mentor', ('be a year-long professional mentor.')),)
+                     ('mentor', ('be a year-long professional mentor.')),
+                     ('other', ('other')),)
 
 
 
