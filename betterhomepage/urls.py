252a859a8ff6712e81da389f4be11893859ce3e1
aliyarahman <aliya.rahman@yahoo.com>
2014-10-31 23:53:32 -0400

    Added jquery div resizer for mobile

--- betterhomepage/urls.py
+++ betterhomepage/urls.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^', include('app.urls')),
     url(r'^app/', include('app.urls')),
-    url(r'^apply/', include('fellowship_app.urls')),
+    url(r'^fellowship_app/', include('fellowship_app.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
